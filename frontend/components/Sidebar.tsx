2347628d350f10d2fa5387d00e39dbdddb5389df
goldinilya-debug <goldin.ilya@gmail.com>
2026-03-12 00:05:56 +0200

    Add Diary import to Sidebar component

--- frontend/components/Sidebar.tsx
+++ frontend/components/Sidebar.tsx
@@ -4,7 +4,8 @@ import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import { useState, useEffect } from 'react'
 import { 
-  LayoutDashboard, 
+  LayoutDashboard,
+  Diary,
   Activity, 
   Search, 
   BarChart3, 
